b53165f8b6a1cb900566bd2f77c06235ba645691
xuefeng <yongxuefeng@aliyun.com>
2019-09-08 10:42:37 +0800

    uodate 01

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,4 +1,10 @@
 public class test {
+    /**
+     * 蓝色：已经提交过又发生修改的、
+     * 红色：还没有提交
+     * 黑色：提交成功
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("i am storage");
         System.out.println("upadte i");
