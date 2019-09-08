139091b3dba2cca061875d1201c5491da10d9450
xuefeng <yongxuefeng@aliyun.com>
2019-09-08 10:37:25 +0800

    uodate 01

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("i am storage");
         System.out.println("upadte i");
+        System.out.println("upadte aaaaaaaa");
+        System.out.println("upadte fffffffffff");
     }
 }
